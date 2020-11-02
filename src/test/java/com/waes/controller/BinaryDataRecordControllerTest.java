442f3cbfbbcdfa3623f680c79cd5d8b92b3accff
Dogus BUKE <dousbuke@gmail.com>
2020-11-03 00:23:17 +0300

    unit tests fix on controller

--- src/test/java/com/waes/controller/BinaryDataRecordControllerTest.java
+++ src/test/java/com/waes/controller/BinaryDataRecordControllerTest.java
@@ -3,10 +3,12 @@ package com.waes.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waes.domain.api.BinaryDataRequest;
+import com.waes.service.BinaryDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +23,9 @@ public class BinaryDataRecordControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private BinaryDataService binaryDataService;
 
     private String asJsonString(final Object obj) {
         try {
